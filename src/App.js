243acf5f76e357166a4a6c46144c75045ec84f25
Christopher Snizik <csnizik@southleft.com>
2021-06-09 15:07:59 -0500

    adding BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
+import React from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import Icon from './patterns/01-atoms/images/Icon';
+
 import Sidebar from './patterns/02-molecules/navigation/Sidebar';
 import classNames from 'classnames';
 
 function App() {
   return (
-    // <div className={classNames('flex','flex-col', 'w-full')}>
-    <div className={classNames('bg-gray-200', 'w-full', 'h-full', 'absolute', 'p-0', 'm-0'
-    ,'overflow-x-hidden')}>
-      <Sidebar />
-      {/* <div>
-        <h1 className="text-5xl font-serif font-bold tracking-tight">
-          Hello World
-        </h1>
-        <Icon type="MagnifyingGlass" color="Crystal-Clear Teal" size="Large" />
-      </div> */}
-    </div>
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
   );
 }
 
