8d6395e1077298769aac1b43aaece7b899c05f8e
Christopher Snizik <csnizik@southleft.com>
2021-06-10 14:10:20 -0500

    rm unused propTypes

--- src/patterns/00-base/Colors/Colors.notastoryyet.js
+++ src/patterns/00-base/Colors/Colors.notastoryyet.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import classNames from 'classnames';
 
 export default {
     title: '00-Base/Colors'
--- src/patterns/02-molecules/blocks/Chart/Chart.js
+++ src/patterns/02-molecules/blocks/Chart/Chart.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import GoogleChart from 'react-google-charts';
 
 import './Chart.css';
