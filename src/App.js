2fa2900633f11f4808dbbeee966c4fbc4194d780
Christopher Snizik <csnizik@southleft.com>
2021-06-10 20:45:05 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from './patterns/05-pages/Home';
 import { Switch, Route } from 'react-router-dom';
 import SignIn from './patterns/05-pages/SignIn';
-import SignUp from './patterns/05-pages/SignUp';
 import ResetPassword from './patterns/05-pages/ResetPassword';
 import Admin from './patterns/05-pages/Admin';
 import PageNotFound from './patterns/05-pages/PageNotFound';
@@ -11,11 +10,8 @@ function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+        <Route exact path="/" component={Home} />
         <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
         <Route path="reset-password" component={ResetPassword} />
         <Route path="/admin" component={Admin} />
         <Route path="*" component={PageNotFound} />
