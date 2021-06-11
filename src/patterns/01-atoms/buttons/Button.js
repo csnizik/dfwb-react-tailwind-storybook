e99733969a8f1cf3c440e04122eb0d19968503ba
Christopher Snizik <csnizik@southleft.com>
2021-06-10 20:42:43 -0500

    button component

--- src/patterns/01-atoms/buttons/Button.js
+++ src/patterns/01-atoms/buttons/Button.js
@@ -1,12 +1,27 @@
+import classNames from 'classnames';
 import React from 'react';
 
 import './Button.css';
 
-const Button = ({...props}) => {
-    return (
-        
-    <button >Hello I am a button</button>        
-    )
-}
+const Button = ({ ...props }) => {
+  return (
+    <button
+      className={classNames(
+          'bg-white',
+          'items-center',
+        'flex',
+        'justify-evenly',
+        'rounded-full',
+        'border',
+        'border-gray-300',
+        'min-w-max',
+        'h-6',
+        'p-2'
+      )}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
